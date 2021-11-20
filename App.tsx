b9a9d1095a6f10be452ec90c8914ff0e1bb05ae1
jeonghoonpark <junghooncentralpark@gmail.com>
2021-11-20 23:32:17 +0900

    스크롤 뷰 추가

--- App.tsx
+++ App.tsx
@@ -1,16 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Text, View } from "./components/Themed";
-import { Button, Linking, StyleSheet } from "react-native";
+import {
+  Button,
+  Linking,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { tSExpressionWithTypeArguments } from "@babel/types";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const [name, setName] = useState("JeonghHoon");
+  const [list, setList] = useState(
+    Array.from({ length: 100 }, (_, i) => ({
+      key: i + 1,
+      name: `${i + 1}`,
+    }))
+  );
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    setRefreshing(true);
+    console.log(list.slice(1));
+    const newList = [{ key: 1, name: "onRefresh" }, ...list.slice(0)];
+    setList(newList);
+    setRefreshing(false);
+  };
 
   if (!isLoadingComplete) {
     return null;
@@ -20,11 +41,35 @@ export default function App() {
         {/* <Navigation colorScheme={colorScheme} />
         <StatusBar /> */}
         <View style={styles.body}>
-          <Text>test</Text>
+          <ScrollView
+            horizontal={false}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                colors={["#FF00FF"]}
+              />
+            }
+          >
+            {list.map((item) => {
+              return (
+                <View style={styles.item} key={item.key}>
+                  <Text>{item.name}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+          <Text>test with {name}</Text>
           <Button
             title="naver"
             onPress={() => {
               Linking.openURL("https://naver.com");
+            }}
+          />
+          <Button
+            title="name"
+            onPress={() => {
+              setName(`test${new Date()}`);
             }}
           />
         </View>
@@ -36,5 +81,11 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     backgroundColor: "#fff123",
+  },
+  item: {
+    backgroundColor: "#4ae1fa",
+    margin: 10,
+    justifyContent: "center",
+    alignContent: "center",
   },
 });
