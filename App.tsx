ea4010773178ea2c81625a011796cb0c11b8510a
jeonghoonpark <junghooncentralpark@gmail.com>
2021-11-20 21:15:51 +0900

    stage 1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Text, View } from "./components/Themed";
+import { Button, Linking, StyleSheet } from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,9 +17,24 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        {/* <Navigation colorScheme={colorScheme} />
+        <StatusBar /> */}
+        <View style={styles.body}>
+          <Text>test</Text>
+          <Button
+            title="naver"
+            onPress={() => {
+              Linking.openURL("https://naver.com");
+            }}
+          />
+        </View>
       </SafeAreaProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: "#fff123",
+  },
+});
