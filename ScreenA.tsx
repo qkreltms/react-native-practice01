06e6650420f0c2918dd011fe3dbf61be43b7ad57
jeonghoonpark <junghooncentralpark@gmail.com>
2021-12-12 21:28:24 +0900

    passing data to other navigation

--- ScreenA.tsx
+++ ScreenA.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Button, StyleSheet, Text, View } from "react-native";
 
-interface ScreeAprops extends NativeStackScreenProps<{ Screen_B: undefined }> {}
-const ScreenA = ({ navigation }: ScreeAprops) => {
+interface ScreeAprops
+  extends NativeStackScreenProps<{ Screen_B: { itemName: string } }> {}
+const ScreenA = ({ navigation, route }: ScreeAprops) => {
+  const { itemName } = route.params as any;
   const onPress = () => {
-    navigation.navigate("Screen_B");
+    // 데이터 넘기기
+    navigation.navigate("Screen_B", { itemName: "Data from Screen A" });
   };
 
   return (
     <View style={styles.body}>
       <Text>Sceen A</Text>
       <Button title="TO SCREEN_B" onPress={onPress} />
+      <Text>{itemName}</Text>
     </View>
   );
 };
