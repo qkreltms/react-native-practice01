792e8f08f670ffcc43e7314a6bb5bfcf1ffe5ea8
jeonghoonpark <junghooncentralpark@gmail.com>
2021-12-26 21:16:29 +0900

    async storage

--- App.tsx
+++ App.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import useCachedResources from "./hooks/useCachedResources";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import ScreenA from "./ScreenA";
-import ScreenB from "./ScreenB";
+import Login from "./Login";
+import Home from "./Home";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,40 +17,9 @@ const App = () => {
     return (
       <SafeAreaProvider>
         <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, size, color }) => {
-                let iconName = "";
-                if (route.name === "Screen_A") {
-                  iconName = "btc";
-                  size = focused ? 25 : 20;
-                } else if (route.name === "Screen_B") {
-                  iconName = "youtube";
-                  size = focused ? 25 : 20;
-                }
-
-                return <FontAwesome5 name={iconName} size={size} />;
-              },
-              // lable 표시 여부
-              tabBarShowLabel: false,
-              tabBarInactiveBackgroundColor: "#999",
-              tabBarActiveBackgroundColor: "#883",
-              // 아래 두 속성 안되는데...
-              tabBarActiveTintColor: "#ff0000",
-              tabBarInactiveTintColor: "#f0f",
-            })}
-          >
-            <Tab.Screen
-              name="Screen_A"
-              component={ScreenA}
-              options={{ tabBarBadge: 3 }}
-              initialParams={{ itemName: "Item from app" }}
-            />
-            <Tab.Screen
-              name="Screen_B"
-              component={ScreenB}
-              initialParams={{ itemName: "Item from app" }}
-            />
+          <Tab.Navigator>
+            <Tab.Screen name="Login" component={Login} />
+            <Tab.Screen name="Home" component={Home} />
           </Tab.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
